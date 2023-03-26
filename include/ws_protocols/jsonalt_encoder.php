b5879e17394b269eaa30578aab05e74a3708f50e
mingan <11653241+mingan666@users.noreply.github.com>
2023-03-26 20:04:40 +0200

    Fix additional format with application/json

---
+++ include/ws_protocols/jsonalt_encoder.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+
+class PwgJsonEncoder extends PwgJsonEncoder
+{
+  function getContentType()
+  {
+    return 'application/json';
+  }
+}
+
+?>
